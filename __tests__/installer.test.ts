04c5d0bc50529706cca86d67abfe4ca758809559
Alif Rachmawadi <arch@subosito.com>
2019-08-13 18:23:00 +0700

    updated test assertion

--- __tests__/installer.test.ts
+++ __tests__/installer.test.ts
@@ -27,7 +27,7 @@ describe('installer tests', () => {
 
   it('Downloads flutter', async () => {
     await installer.getFlutter('v1.7.8+hotfix.4', 'stable');
-    const sdkDir = path.join(toolDir, 'Flutter', 'v1.7.8+hotfix.4-stable');
+    const sdkDir = path.join(toolDir, 'Flutter', '1.7.8', 'x64');
 
     expect(fs.existsSync(`${sdkDir}.complete`)).toBe(true);
     expect(fs.existsSync(path.join(sdkDir, 'bin'))).toBe(true);
@@ -35,7 +35,7 @@ describe('installer tests', () => {
 
   it('Downloads flutter from beta channel', async () => {
     await installer.getFlutter('v1.8.3', 'beta');
-    const sdkDir = path.join(toolDir, 'Flutter', 'v1.8.3-beta');
+    const sdkDir = path.join(toolDir, 'Flutter', '1.8.3', 'x64');
 
     expect(fs.existsSync(`${sdkDir}.complete`)).toBe(true);
     expect(fs.existsSync(path.join(sdkDir, 'bin'))).toBe(true);
